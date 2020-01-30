d04d8e01cc9b497722763a8d8d22c268d2330a8b
mehdi najafian <najafian@live.com>
2020-01-30 12:33:01 +0330

    This is a initial assignment

---
+++ src/main/java/org/interview/oauth/twitter/TwitterAuthenticationException.java
@@ -0,0 +1,21 @@
+package org.interview.oauth.twitter;
+
+
+public class TwitterAuthenticationException extends Exception {
+
+	public TwitterAuthenticationException() {
+		super();
+	}
+	
+	public TwitterAuthenticationException(final String message) {
+		super(message);
+	}
+	
+	public TwitterAuthenticationException(final String message, final Throwable t) {
+		super(message, t);
+	}
+	
+	public TwitterAuthenticationException(final Throwable t) {
+		super(t);
+	}
+}
